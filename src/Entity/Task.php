c699f7d20d34447e760202e2b35190e835e626fb
seb <contact@smaloron.eu>
2023-03-31 14:14:58 +0200

    Groupes de normalisation

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,18 +3,28 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\TaskRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 #[ApiResource(
     operations: [
-        new GetCollection(),
-        new GET()
-    ]
+        new GetCollection(uriTemplate: '/taches'),
+        new GET(uriTemplate: '/taches/{id}'),
+        new POST(uriTemplate: '/taches'),
+        new PUT(uriTemplate: '/taches/{id}'),
+        new PATCH(uriTemplate: '/taches/{id}'),
+        new DELETE(uriTemplate: '/taches/{id}')
+    ],
+    normalizationContext: ['groups' => ['task--show']]
 )]
 class Task
 {
@@ -24,12 +34,14 @@ class Task
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['task--show'])]
     private ?string $taskName = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $createdAt = null;
 
     #[ORM\Column]
+    #[Groups(['task--show'])]
     private ?bool $done = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
@@ -94,5 +106,12 @@ class Task
         $this->dueAt = $dueAt;
 
         return $this;
+    }
+
+    #[Groups(['task--show'])]
+    public function isNew(){
+        $now = new \DateTime();
+        $interval = $this->createdAt->diff($now);
+        return $interval->d <= 2;
     }
 }
