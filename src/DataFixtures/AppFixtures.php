3b67871f52d2341b5582f3c86432a67afd0c95fc
seb <contact@smaloron.eu>
2023-03-31 10:26:51 +0200

    Mise en place du projet avec une entité Task

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\TaskFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        TaskFactory::createMany(50);
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $taskName = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column]
+    private ?bool $done = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dueAt = null;
+
+    public function __construct()
+    {
+        // Valeur par défaut à la création d'une tâche
+        $this->done = false;
+        $this->createdAt = new \DateTime();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTaskName(): ?string
+    {
+        return $this->taskName;
+    }
+
+    public function setTaskName(string $taskName): self
+    {
+        $this->taskName = $taskName;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function isDone(): ?bool
+    {
+        return $this->done;
+    }
+
+    public function setDone(bool $done): self
+    {
+        $this->done = $done;
+
+        return $this;
+    }
+
+    public function getDueAt(): ?\DateTimeInterface
+    {
+        return $this->dueAt;
+    }
+
+    public function setDueAt(?\DateTimeInterface $dueAt): self
+    {
+        $this->dueAt = $dueAt;
+
+        return $this;
+    }
+}
